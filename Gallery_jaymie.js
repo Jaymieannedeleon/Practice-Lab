e2558e85a9a459fe5cc7923f8e6703c5ec14fe7b
Jaymieannedeleon <60497231+Jaymieannedeleon@users.noreply.github.com>
2020-02-16 12:50:57 +0800

    Add files via upload

---
+++ Gallery_jaymie.js
@@ -0,0 +1,13 @@
+function upDate(previewPic){
+    var picture = previewPic.getAttribute( "src" );
+    var alt = previewPic.getAttribute( "alt" );
+    document.getElementById('image').style.backgroundImage = "url('" + picture + "')";
+    document.getElementById('image').innerHTML = alt;
+ }
+ 
+   function unDo(a){
+    a = document.getElementById('image');
+    a.style.backgroundImage = "url('')";
+     document.getElementById('image').innerHTML = "Hover over an image below to display here";
+   
+ }
